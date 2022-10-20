c36efb2d98ac49778741c38b734a135e95902d19
Prakxo <87568477+Prakxo@users.noreply.github.com>
2022-10-20 10:03:35 +0200

    Implemented time rand.

--- createc.c
+++ createc.c
@@ -4,12 +4,15 @@ int main(){
      int min = -32768;
      int max = 32767;
     printf("#include <stdio.h> \n");
+    printf("#include <stdlib.h> \n");
+    printf("#include <time.h> \n");
     printf("int main(){\n");
-    printf("int valor = 10; \n");
-    printf("switch(valor){ \n");
+    printf("srand(time(NULL));");
+    printf("int nu = (rand() %% (%d +1 - %d  ) + %d);", max, min, min);
+    printf("switch(nu){ \n");
     for (int i = min; i<= max; i++ ){
-        printf("case %i: \n", i); //cba casting to short
-        printf("printf(\"%i\"); \n", i);
+        printf("case %i: \n", i); //cba casting to short, changed variables instead.
+        printf("printf(\"%i\\n\"); \n", i);
         printf("break; \n");
     }
     printf("}};");
